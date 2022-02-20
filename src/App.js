26648b1669fd21ac09608cf7da05629ab905dc21
Shah <shebiraaja@gmail.com>
2022-02-20 14:36:25 +0100

    conseguimos el inicio de sesion con boton, falta hacer el display cuando es true

--- src/App.js
+++ src/App.js
@@ -7,11 +7,31 @@ function App() {
   const [username, setUsername] = useState()
   const [password, setPassword] = useState()
   const [signIn, setSignIn] = useState()
-  const [signOut, setSignOut] = useState()
+  const [signOut, setSignOut] = useState(true)
 
   // const handleReset = () => {
   //   document.querySelectorAll('input').event.target.value = ""
   // };
+
+  // const check = (username, password) => {
+  //   if(username === "shebi" && password === "shebi"){
+  //     <SigninPage/> 
+  //   }else {
+  //     <p>no</p>
+  //   }
+
+  // }
+
+
+  function hello(username, password) {
+    // alert(`hello ${username} ${password}`)
+    if (username === "shebi" && password === "shebi") {
+      console.log("has iniciado sesion")
+    } else {
+      console.log("credenciales incorrectas")
+    }
+  }
+
 
   return (
 
@@ -20,13 +40,13 @@ function App() {
       <input type="text" onChange={event => setUsername(event.target.value)} />
       <input type="password" onChange={event => setPassword(event.target.value)} />
 
-      {username === "shebi" && password === "1234" ? <SigninPage /> : <p>no tienes acceso</p> }
+      {/*username === "shebi" && password === "1234" ? <SigninPage /> : <p>no tienes acceso</p>*/}
 
+
+      <button onClick={() => hello(username, password)}>sign in</button>
+      <button onClick={() => setSignOut(false)}>sign out</button>
       {console.log(username)}
       {console.log(password)}
-
-      <button onClick={() => setSignIn("")}>sign in</button>
-      <button onClick={() => setSignOut("")}>sign out</button>
 
 
       {/*signIn === true ? <SigninPage /> : ""*/}
