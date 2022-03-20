ffc235204167b39c98947e22ad0be2bce86ef578
Shah <shebiraaja@gmail.com>
2022-03-20 15:28:43 +0100

    trying the form validation installing packages

--- src/App.js
+++ src/App.js
@@ -1,81 +1,70 @@
 import React, { useState } from 'react';
+import { useForm } from 'react-hook-form';
 import './App.css';
 import SigninPage from './SigninPage';
 
 function App() {
+  const { register, handleSubmit, errors} = useForm()
 
-  const [username, setUsername] = useState()
-  const [password, setPassword] = useState()
-  const [signIn, setSignIn] = useState()
-  const [signOut, setSignOut] = useState(true)
+  // const [username, setUsername] = useState()
+  // const [password, setPassword] = useState()
+  // const [signIn, setSignIn] = useState()
+  // const [signOut, setSignOut] = useState(true)
 
-  // const handleReset = () => {
-  //   document.querySelectorAll('input').event.target.value = ""
-  // };
+  // function entrar(username, password) {
+  //   if (username === "shebi_32" && password === "1234") {
+  //     alert("has iniciado sesion")
+  //   } else {
+  //     console.log("credenciales incorrectas")
+  //     alert("Credenciales incorrectas")
 
-  // const check = (username, password) => {
-  //   if(username === "shebi" && password === "shebi"){
-  //     <SigninPage/> 
-  //   }else {
-  //     <p>no</p>
   //   }
-
   // }
 
-
-  function entrar(username, password) {
-    // alert(`hello ${username} ${password}`)
-    if (username === "shebi_32" && password === "1234") {
-      // console.log("has iniciado sesion")
-      console.log(<SigninPage/>)
-    } else {
-      console.log("credenciales incorrectas")
-    }
-  }
-
-  // function salir() {
-  //   console.log(document.getElementsByClassName("input").value = "")
-
-
-
-  //   // if (username === "" && password === "") {
-  //   //   console.log("has cerrando sesion")
-  //   // }
-  // }
-
-  // function clear(username, password) {
-  //   if(username === String & password === String) {
-  //     username(() => "") && password(() => "")
-  //   } 
-  // }
-
-  function clear(username, password) {
-    if(username === "" & password === "") {
-      username(() => "") && password(() => "")
-    } 
-  }
 
   return (
 
-    <div>
-
-      <input type="text" className="input" onChange={event => setUsername(event.target.value) } value={username} />
-      <input type="password" className="input" onChange={event => setPassword(event.target.value)} value={password} />
-
-      {/*username === "shebi" && password === "1234" ? <SigninPage /> : <p>no tienes acceso</p>*/}
-
-
-      <button onClick={/*() => setSignIn()*/() => entrar(username, password)}>sign in</button>
-      <button onClick={
-        /*() => setUsername(() => "") && () => setPassword(() => "")*/
-      () => clear(username, password)}>sign out</button>
-      {console.log(username)}
-      {console.log(password)}
-
-
-      {/*signIn === true ? <SigninPage /> : ""*/}
-      {/*signIn === true ? <SigninPage /> : ""*/}
+    <form>
+      <h1>Registration form</h1>
+      <div className="field">
+        <label>Username</label>
+        <input type="text" name='username' placeholder='username' ref={register} />
       </div>
+
+      <div className="field">
+        <label>Username</label>
+        <input type="email" name='email' placeholder='email' ref={register} />
+      </div>
+
+      <div className="field">
+        <label>Username</label>
+        <input type="password" name='password' placeholder='password' ref={register} />
+      </div>
+      <button>Submit</button>
+    </form>
+
+
+    // <div>
+
+    //   <input type="text" className="input" onChange={event => setUsername(event.target.value) } value={username} />
+    //   <input type="password" className="input" onChange={event => setPassword(event.target.value)} value={password} />
+
+    //   {/*username === "shebi" && password === "1234" ? <SigninPage /> : <p>no tienes acceso</p>*/}
+
+
+    //   <button onClick={/*() => setSignIn()*/() => entrar(username, password)}>sign in</button>
+    //   <button onClick={
+    //     /*() => setUsername(() => "") && () => setPassword(() => "")*/
+    //   () => clear(username, password)}>sign out</button>
+    //   {console.log(username)}
+    //   {console.log(password)}
+
+
+    //   {/*signIn === true ? <SigninPage /> : ""*/}
+    //   {/*signIn === true ? <SigninPage /> : ""*/}
+    //   </div>
+
+
 
   );
 }
