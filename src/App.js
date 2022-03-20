3587a8eec5f5a920c99f2bb7f73ad60237489e7c
Shah <shebiraaja@gmail.com>
2022-03-20 16:48:33 +0100

    cambiamos el codigo

--- src/App.js
+++ src/App.js
@@ -4,7 +4,11 @@ import './App.css';
 import SigninPage from './SigninPage';
 
 function App() {
-  const { register, handleSubmit, errors} = useForm()
+  const { register, handleSubmit, errors } = useForm()
+  const [userInfo, setUserInfo] = useState();
+  const onSubmit = (data) => {
+    setUserInfo(data)
+  }
 
   // const [username, setUsername] = useState()
   // const [password, setPassword] = useState()
@@ -24,45 +28,49 @@ function App() {
 
   return (
 
-    <form>
-      <h1>Registration form</h1>
-      <div className="field">
-        <label>Username</label>
-        <input type="text" name='username' placeholder='username' ref={register} />
-      </div>
+    <div>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <pre>{JSON.stringify(userInfo, undefined, 2)}</pre>
+        <h1>Registration form</h1>
+        <div className="field">
+          <label>Username</label>
+          <input type="text" name='username' placeholder='username' ref={register} />
+        </div>
 
-      <div className="field">
-        <label>Username</label>
-        <input type="email" name='email' placeholder='email' ref={register} />
-      </div>
+        <div className="field">
+          <label>Email</label>
+          <input type="email" name='email' placeholder='email' ref={register} />
+        </div>
 
-      <div className="field">
-        <label>Username</label>
-        <input type="password" name='password' placeholder='password' ref={register} />
-      </div>
-      <button>Submit</button>
-    </form>
+        <div className="field">
+          <label>Password</label>
+          <input type="password" name='password' placeholder='password' ref={register} />
+        </div>
+        <button>Submit</button>
+      </form>
 
+    </div>
 
-    // <div>
+    /* <div>
 
-    //   <input type="text" className="input" onChange={event => setUsername(event.target.value) } value={username} />
+    //   <input type="text" className="input" onChange={event => setUsername(event.target.value)} value={username} />
     //   <input type="password" className="input" onChange={event => setPassword(event.target.value)} value={password} />
 
-    //   {/*username === "shebi" && password === "1234" ? <SigninPage /> : <p>no tienes acceso</p>*/}
+    //   {/*username === "shebi" && password === "1234" ? <SigninPage /> : <p>no tienes acceso</p>}
 
 
-    //   <button onClick={/*() => setSignIn()*/() => entrar(username, password)}>sign in</button>
+    //   <button onClick={/*() => setSignIn()() => entrar(username, password)}>sign in</button>
     //   <button onClick={
-    //     /*() => setUsername(() => "") && () => setPassword(() => "")*/
+    //     /*() => setUsername(() => "") && () => setPassword(() => "")
     //   () => clear(username, password)}>sign out</button>
     //   {console.log(username)}
     //   {console.log(password)}
 
 
-    //   {/*signIn === true ? <SigninPage /> : ""*/}
-    //   {/*signIn === true ? <SigninPage /> : ""*/}
+    //   {/*signIn === true ? <SigninPage /> : ""}
+    //   {/*signIn === true ? <SigninPage /> : ""}
     //   </div>
+  */
 
 
 
